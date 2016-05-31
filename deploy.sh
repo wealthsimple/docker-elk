a1c95dd8e7edaca3feb48bd05a734541da3187fb
Marco Costa <mmarcottulio@gmail.com>
2016-05-31 15:26:19 -0400

    Add CircleCI for deployments

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+PROJECT=$(basename $0)
+
+docker-machine scp -r . logging-docker:$PROJECT
+PWD=/home/ubuntu/$PROJECT/ docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --force-recreate
