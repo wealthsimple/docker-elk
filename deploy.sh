7b4ef38e2e5c94730c15c711b25ae7e476d90cb6
Marco Costa <mmarcottulio@gmail.com>
2016-05-31 18:19:04 -0400

    Remove testing code

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,4 @@
 PROJECT="docker-elk"
 
-echo "pathh!!!"
-echo $DOCKER_CERT_PATH
-
-echo "./docker-machine scp -r . logging-docker:$PROJECT"
-
 ./docker-machine scp -r . logging-docker:$PROJECT
 PWD=/home/ubuntu/$PROJECT/ docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --force-recreate
